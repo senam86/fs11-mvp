e4cfb04a78c01956a69c9772972812cd60f82585
senam86 <senameagblevon@gmail.com>
2021-02-16 19:20:12 +0100

    endpoints in progress

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var insightsRouter = require('./routes/insights');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/insights', insightsRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
+const db = require("../model/helper");
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  db(`SELECT * FROM users`) 
+    .then(results => {
+      res.send(results.data);
+    })
+  
 });
+
+router.get('/:id', function(req, res, next) {
+  const Id = req.params.id;
+  // const {id} = req.params;
+  db(`SELECT * FROM users WHERE userId=${Id};`)
+    .then(results => {
+      res.send(results.data);
+    })
+  
+});
+
+// router.post('/', function())
 
 //create user profile
 
