30308f2f4549c17cbae456ecb8e9c38d6c30be72
senam86 <senameagblevon@gmail.com>
2021-02-16 23:26:44 +0100

    users.js complete

--- model/database.js
+++ model/database.js
@@ -10,7 +10,7 @@ const con = mysql.createConnection({
   host: DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
   password: DB_PASS,
-  database: DB_NAME || "facebook",
+  database: DB_NAME || "mvp",
   multipleStatements: true
 });
 
@@ -19,10 +19,10 @@ con.connect(function(err) {
   console.log("Connected!");
 
   let sql =
-    "DROP TABLE if exists students; CREATE TABLE students(id INT NOT NULL AUTO_INCREMENT, firstname VARCHAR(40) not null, lastname VARCHAR(40) not null, PRIMARY KEY (id));";
+    "CREATE TABLE `users` (`userId` INTEGER NO NULL PRIMARY KEY AUTO_INCREMENT, `firstname` VARCHAR(255), `lastname` VARCHAR(255), `email` VARCHAR(255), `username` VARCHAR(255), `currentlocation`); CREATE TABLE `insights` (`userId` INTEGER NOT NULL, `title` VARCHAR(255), `category` VARCHAR(255), `description` VARCHAR(255), `ratings` VARCHAR(255));";
   con.query(sql, function(err, result) {
     if (err) throw err;
-    console.log("Table creation `students` was successful!");
+
 
     console.log("Closing...");
   });
--- routes/users.js
+++ routes/users.js
@@ -7,22 +7,52 @@ router.get('/', function(req, res, next) {
     .then(results => {
       res.send(results.data);
     })
-  
+  .catch((err) => res.status(500).send(err));
 });
 
 router.get('/:id', function(req, res, next) {
   const Id = req.params.id;
   // const {id} = req.params;
+  try {
   db(`SELECT * FROM users WHERE userId=${Id};`)
     .then(results => {
       res.send(results.data);
-    })
-  
+    });
+  } catch (error) {
+    res.status(500).send(err);
+  }
 });
 
-// router.post('/', function())
+router.post('/', function(req, res, next) {
+  const {firstname, lastname, email, username, currentlocation} = req.body;
+  db(`INSERT INTO users (firstname, lastname, email, username, currentlocation) VALUES ("${firstname}", "${lastname}", "${email}", "${username}", "${currentlocation}");`).then(results => {
+    res.send("new entry was added");
+  })
+  .catch((err) => res.status(500).send(err));
+});
 
+router.put('/ :id', function(req, res, next) {
+  const {firstname, lastname, email, username, currentlocation} = req.body;
+  try {
+    db(`UPDATE users SET (firstname, lastname, email, username, currentlocation) VALUES ("${firstname}", "${lastname}", "${email}", "${username}", "${currentlocation}");`).then(results => {
+      db(`SELECT * FROM users;`);
+      res.send(results.data);
+    });
+  } catch (error) {
+    res.status(500).send(err);
+  }
+});
 
-
+router.delete('/ :id', function(req, res, next) {
+  const Id = req.params.id;
+  try {
+    db(`DELETE FROM users WHERE userId=${Id};`).then(results => {
+      db(`SELECT * FROM users;`);
+      res.send({message: "User was deleted"});
+    })
+  } catch (error) {
+    res.status(500).send(err);
+  }
+})
 
 module.exports = router;
