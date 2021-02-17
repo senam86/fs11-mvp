22a6be2df8633e51dffceb75348bcdfcde45bf90
senam86 <senameagblevon@gmail.com>
2021-02-17 10:50:27 +0100

    insights in progress

--- routes/insights.js
+++ routes/insights.js
@@ -1,10 +1,61 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db(`SELECT * FROM insights`) 
+    .then(results => {
+      res.send(results.data);
+    })
+  .catch((err) => res.status(500).send(err));
 });
+
+router.get('/:id', function(req, res, next) {
+  const Id = req.params.id;
+  try {
+  db(`SELECT * FROM insights WHERE userId=${Id};`)
+    .then(results => {
+      res.send(results.data);
+    });
+  } catch (error) {
+    res.status(500).send(err);
+  }
+});
+
+router.post('/', function(req, res, next) {
+  const {firstname, lastname, email, username, currentlocation} = req.body;
+  db(`INSERT INTO users (firstname, lastname, email, username, currentlocation) VALUES ("${firstname}", "${lastname}", "${email}", "${username}", "${currentlocation}");`).then(results => {
+    res.send("new entry was added");
+  })
+  .catch((err) => res.status(500).send(err));
+});
+
+router.put('/ :id', function(req, res, next) {
+  const {firstname, lastname, email, username, currentlocation} = req.body;
+  try {
+    db(`UPDATE users SET (firstname, lastname, email, username, currentlocation) VALUES ("${firstname}", "${lastname}", "${email}", "${username}", "${currentlocation}");`).then(results => {
+      db(`SELECT * FROM users;`);
+      res.send(results.data);
+    });
+  } catch (error) {
+    res.status(500).send(err);
+  }
+});
+
+router.delete('/ :id', function(req, res, next) {
+  const Id = req.params.id;
+  try {
+    db(`DELETE FROM users WHERE userId=${Id};`).then(results => {
+      db(`SELECT * FROM users;`);
+      res.send({message: "User was deleted"});
+    })
+  } catch (error) {
+    res.status(500).send(err);
+  }
+})
+ 
+
 
 
 
